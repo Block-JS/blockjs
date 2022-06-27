418cecce87efef87d5166c08621f8d20a961c2ad
Violet Hayes <ampc11234@gmail.com>
2022-06-27 09:36:53 -0700

    Made it so JSRunner.java can read files and added test.js for testing the class

--- src/main/java/com/violeth/blockjs/blockjs/jsinterface/runtime/JSRunner.java
+++ src/main/java/com/violeth/blockjs/blockjs/jsinterface/runtime/JSRunner.java
@@ -1,20 +1,40 @@
 package com.violeth.blockjs.blockjs.jsinterface.runtime;
 
 import com.eclipsesource.v8.V8;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class JSRunner {
     private static final V8 runner = V8.createV8Runtime();
 
-    public File file;
+    public static File file;
 
     public JSRunner(File file) {
-        this.file = file;
+        JSRunner.file = file;
+    }
+
+    public static @NotNull String getFileText() {
+        try {
+            Scanner scanner = new Scanner(file);
+
+            StringBuilder sb = new StringBuilder();
+
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine());
+            }
+
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("ENOENT: no such file or directory");
+        }
     }
 
     public static void main(String[] args) {
-        Object result = runner.executeScript("'Hello World!'");
-        System.out.println(result);
+        String fileText = getFileText();
+
+        Object result = runner.executeScript(fileText);
     }
 }
