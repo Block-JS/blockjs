23056397c7f1c01838a268f00c5d73080f696572
Violet Hayes <ampc11234@gmail.com>
2022-06-29 14:14:16 -0700

    Created command for interfacing with plugin

--- src/main/java/com/violeth/blockjs/blockjs/BlockJS.java
+++ src/main/java/com/violeth/blockjs/blockjs/BlockJS.java
@@ -1,13 +1,16 @@
 package com.violeth.blockjs.blockjs;
 
+import com.violeth.blockjs.blockjs.commands.CommandInterface;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class BlockJS extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Objects.requireNonNull(this.getCommand("blockjs")).setExecutor(new CommandInterface());
     }
 
     @Override
