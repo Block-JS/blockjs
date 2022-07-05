d85403caaaf0b6f429052fb8e778cb5496c24448
TheGreatViolet <ampc11234@gmail.com>
2022-07-04 17:13:33 -0700

    Implemented method for whispering to player

--- src/npm-package/src/chat/index.ts
+++ src/npm-package/src/chat/index.ts
@@ -1,8 +1,16 @@
+import {Player} from "../entities/player";
+
 function broadcast(message: string) {
   // @ts-ignore
     javaBroadcast(message);
 }
 
+function whisper(player: Player, message: string) {
+  // @ts-ignore
+    javaWhisper(player.name, message);
+}
+
 export default {
     broadcast,
+    whisper,
 }
