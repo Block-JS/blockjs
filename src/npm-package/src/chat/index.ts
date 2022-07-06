caf215d2dbc65a8087857b4c95042b6ee3f05723
TheGreatViolet <ampc11234@gmail.com>
2022-07-05 21:24:52 -0700

    Added JSDoc comments to js side functions

--- src/npm-package/src/chat/index.ts
+++ src/npm-package/src/chat/index.ts
@@ -1,10 +1,19 @@
 import {Player} from "../entities/player";
 
+/**
+ * Broadcasts a message to the chat.
+ * @param {string} message - Message to broadcast.
+ */
 function broadcast(message: string) {
   // @ts-ignore
     javaBroadcast(message);
 }
 
+/**
+ * Sends a message to a specific player.
+ * @param {Player} player - Player to send message to.
+ * @param {string} message - Message to send.
+ */
 function whisper(player: Player, message: string) {
   // @ts-ignore
     javaWhisper(player.name, message);
--- src/npm-package/src/entities/player.ts
+++ src/npm-package/src/entities/player.ts
@@ -1,3 +1,8 @@
+/**
+ * Gets a player and returns it as a Player object.
+ * @param {string} name - Name of the player.
+ * @returns {Player} - Player object.
+ */
 function getPlayer(name: string): Player {
     // @ts-ignore
     const obj: object = javaGetPlayer(name);
@@ -5,6 +10,10 @@ function getPlayer(name: string): Player {
     return obj as Player;
 }
 
+/**
+ * Gets a list of all online players.
+ * @returns {Player[]} - Array of Player objects.
+ */
 function getOnlinePlayers(): Player[] {
     // @ts-ignore
     const obj: object[] = javaGetOnlinePlayers();
@@ -12,11 +21,21 @@ function getOnlinePlayers(): Player[] {
     return obj as Player[];
 }
 
+/**
+ * Damages a player.
+ * @param {Player} player - Player to damage.
+ * @param {number} damage - Amount of damage to deal.
+ */
 function damagePlayer(player: Player, damage: number): void {
     // @ts-ignore
     javaDamagePlayer(player.name, damage);
 }
 
+/**
+ * Heals a player.
+ * @param {Player} player - Player to heal.
+ * @param {number} health - Amount of health to restore.
+ */
 function healPlayer(player: Player, health: number): void {
     // @ts-ignore
     javaHealPlayer(player.name, health);
