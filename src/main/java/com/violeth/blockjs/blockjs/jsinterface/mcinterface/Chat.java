4bc5646d8d5d4fb128defe4726db27d1043a35cd
TheGreatViolet <ampc11234@gmail.com>
2022-07-08 12:30:17 -0700

    Added decorators and updated method names in various MCInterface classes

--- src/main/java/com/violeth/blockjs/blockjs/jsinterface/mcinterface/Chat.java
+++ src/main/java/com/violeth/blockjs/blockjs/jsinterface/mcinterface/Chat.java
@@ -1,19 +1,23 @@
 package com.violeth.blockjs.blockjs.jsinterface.mcinterface;
 
+import com.caoccao.javet.annotations.V8Function;
 import org.bukkit.Bukkit;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class Chat {
-    public static Object broadcast(String message) {
+    @V8Function
+    public static Object javaBroadcast(String message) {
         Bukkit.getOnlinePlayers().forEach(player -> {
             player.sendMessage(message);
         });
+
         return null;
     }
 
-    public static void whisper(String message, @NotNull Player player) {
+    @V8Function
+    public static void javaWhisper(String message, @NotNull Player player) {
         player.sendMessage(Component.text(message));
     }
 }
--- src/main/java/com/violeth/blockjs/blockjs/jsinterface/mcinterface/entitys/Players.java
+++ src/main/java/com/violeth/blockjs/blockjs/jsinterface/mcinterface/entitys/Players.java
@@ -1,29 +1,34 @@
 package com.violeth.blockjs.blockjs.jsinterface.mcinterface.entitys;
 
+import com.caoccao.javet.annotations.V8Function;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
 
 public class Players {
-    public Player getPlayer(String name) {
+    @V8Function
+    public Player javaGetPlayer(String name) {
         return Bukkit.getPlayer(name);
     }
 
-    public Collection<? extends Player> getOnlinePlayers() {
+    @V8Function
+    public Collection<? extends Player> javaGetOnlinePlayers() {
         return Bukkit.getOnlinePlayers();
     }
 
+    @V8Function
     public void damage(String name, double damage) {
-        Player player = getPlayer(name);
+        Player player = javaGetPlayer(name);
 
         if (player != null) {
             player.damage(damage);
         }
     }
 
+    @V8Function
     public void heal(String name, double heal) {
-        Player player = getPlayer(name);
+        Player player = javaGetPlayer(name);
 
         if (player != null) {
             player.setHealth(player.getHealth() + heal);
