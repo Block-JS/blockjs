df5410e68715b84bbe1f58598d0749a863248946
Awesome1221 <109266311+Awesome1221@users.noreply.github.com>
2022-07-15 23:55:15 +0000

    Improved command handling and added resource filtering

---
+++ src/main/java/com/violeth/blockjs/blockjs/commands/VersionCommand.java
@@ -0,0 +1,22 @@
+package com.violeth.blockjs.blockjs.commands;
+
+import com.violeth.blockjs.blockjs.BlockJS;
+import com.violeth.blockjs.blockjs.jsinterface.JSRunner;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.command.CommandSender;
+
+public class VersionCommand extends Command {
+    {
+        key = "version";
+    }
+    @Override
+    public void handle(CommandSender sender, String[] args) {
+        sender.sendMessage(BlockJS.getPluginMessagePrefix().append(
+            Component.text("The current version: ").append(
+                Component.text(BlockJS.instance.getDescription().getVersion()).color(TextColor.color(0xDDDDDD)).decorate(TextDecoration.ITALIC)
+            )
+        ));
+    }
+}
