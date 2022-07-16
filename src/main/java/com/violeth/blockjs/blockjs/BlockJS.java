41479498a2c020569f5523a8a06e148b5d961106
Awesome1221 <109266311+Awesome1221@users.noreply.github.com>
2022-07-16 10:32:18 +0000

    Updated plugin's main class

--- src/main/java/com/violeth/blockjs/blockjs/BlockJS.java
+++ src/main/java/com/violeth/blockjs/blockjs/BlockJS.java
@@ -1,15 +1,25 @@
 package com.violeth.blockjs.blockjs;
 
-import com.violeth.blockjs.blockjs.commands.CommandInterface;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
 public final class BlockJS extends JavaPlugin {
+    static public BlockJS instance;
+
+    static public Component getPluginMessagePrefix() {
+        return Component.text("[")
+                .append(Component.text(BlockJS.instance.getName()).color(TextColor.color(0x00BBFF)))
+                .append(Component.text("] "));
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+
         Objects.requireNonNull(this.getCommand("blockjs")).setExecutor(new CommandInterface());
     }
 
