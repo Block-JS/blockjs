0c5388229842b2f53a2be7657bb55ebb5a42c9d5
Awesome1221 <109266311+Awesome1221@users.noreply.github.com>
2022-10-20 12:32:52 +0000

    Added JSInterfaceTest back

---
+++ src/test/java/com/violeth/blockjs/blockjs/jsinterface/JSInterfaceTest.java
@@ -0,0 +1,38 @@
+package com.violeth.blockjs.blockjs.jsinterface;
+
+import com.violeth.blockjs.blockjs.JSRuntimeManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+/** Note: might not work yet */
+class JSInterfaceTest {
+    @Test
+    @DisplayName("Runs JS without error")
+    void runJS() {
+        var node = JSRuntimeManager.createNodeJS();
+
+        assertDoesNotThrow(() -> node.exec(new File("src/test/resources/jsTests/basicTest.js")));
+    }
+
+//    @Test
+//    @DisplayName("Can register interface")
+//    void registerCallbacks() {
+//        var node = JSRuntimeManager.createNodeJS();
+//
+//        node.exec(new File("src/test/resources/jsTests/callbackTest.js"));
+//
+//        assertDoesNotThrow(() -> runner.registerCallbacks());
+//    }
+
+    @Test
+    @DisplayName("Can run JS with interface methods")
+    void runJSWithInterfaceMethods() {
+        var node = JSRuntimeManager.createNodeJS();
+
+        assertDoesNotThrow(() -> node.exec(new File("src/test/resources/jsTests/interfaceTest.js")));
+    }
+}
