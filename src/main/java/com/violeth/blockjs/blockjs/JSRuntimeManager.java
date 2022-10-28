d14b9264129520b5ba06a5446e94380375217bb9
Awesome1221 <109266311+Awesome1221@users.noreply.github.com>
2022-10-28 14:06:26 +0000

    Registered the new methods to the runtime

--- src/main/java/com/violeth/blockjs/blockjs/JSRuntimeManager.java
+++ src/main/java/com/violeth/blockjs/blockjs/JSRuntimeManager.java
@@ -4,6 +4,7 @@ import com.eclipsesource.v8.*;
 import com.violeth.blockjs.blockjs.jsinterface.mcinterface.Chat;
 import com.violeth.blockjs.blockjs.jsinterface.mcinterface.Player;
 import com.violeth.blockjs.blockjs.jsinterface.mcinterface.World;
+import org.bukkit.GameMode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,60 @@ public class JSRuntimeManager {
         {
             var player = new Player(runtime);
             var playerV8 = new V8Object(runtime);
+
+            playerV8.registerJavaMethod(player, "setGameMode", "setGameMode", new Class[] {
+                String.class,
+                GameMode.class
+            });
+
+            playerV8.registerJavaMethod(player, "setWalkSpeed", "setWalkSpeed", new Class[] {
+                String.class,
+                float.class
+            });
+
+            playerV8.registerJavaMethod(player, "setFlySpeed", "setFlySpeed", new Class[] {
+                String.class,
+                float.class
+            });
+
+            playerV8.registerJavaMethod(player, "setFlying", "setFlying", new Class[] {
+                String.class,
+                boolean.class
+            });
+
+            playerV8.registerJavaMethod(player, "setSneaking", "setSneaking", new Class[] {
+                String.class,
+                boolean.class
+            });
+
+            playerV8.registerJavaMethod(player, "setSprinting", "setSprinting", new Class[] {
+                String.class,
+                boolean.class
+            });
+
+            playerV8.registerJavaMethod(player, "setHealth", "setHealth", new Class[] {
+                String.class,
+                double.class
+            });
+
+            playerV8.registerJavaMethod(player, "setExhaustion", "setExhaustion", new Class[] {
+                String.class,
+                float.class
+            });
+
+            playerV8.registerJavaMethod(player, "setPosition", "setPosition", new Class[] {
+                String.class,
+                double.class,
+                double.class,
+                double.class
+            });
+
+            playerV8.registerJavaMethod(player, "setVelocity", "setVelocity", new Class[] {
+                String.class,
+                double.class,
+                double.class,
+                double.class
+            });
 
             playerV8.registerJavaMethod(player, "getOnlinePlayerUUIDByName", "getOnlinePlayerUUIDByName", new Class[] {
                 String.class
