2f391b120a32b12d9e57e7bb162b84524e6d6b49
Violet Hayes <ampc11234@gmail.com>
2023-09-05 21:57:49 -0700

    Hopefully fixed deployment to pages

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   title: 'BlockJS',
   tagline: 'Use JavaScript to interact with Minecraft',
   url: 'https://block-js.github.io',
-  baseUrl: '/documentation/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -56,7 +56,7 @@ const config = {
             label: 'Docs',
           },
           {
-            to: 'blog',
+            to: '/blog',
             label: 'Blog',
             position: 'left'
           },
